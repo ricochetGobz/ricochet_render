d1b6f3a2be7de8f16e6cf722ad83f36f917178c5
Jérémie Boulay <jeremi.boulay@gmail.com>
2016-06-02 10:52:30 +0200

    :construction: draggably

--- app/core/scene/Scene.js
+++ app/core/scene/Scene.js
@@ -5,6 +5,7 @@
 **/
 
 import { pixi as Pixi } from 'pixi.js';
+import interact from 'interact.js';
 
 export default class Scene {
 
@@ -14,15 +15,49 @@ export default class Scene {
    */
   constructor( _width, _height ) {
 
-    this.width = _width;
-    this.height = _height;
+    this.width = _width - 100;
+    this.height = _height - 100;
+
+    // this.width = 680;
+    // this.height = 480;
 
     this.children = [];
 
     this.stage = new PIXI.Stage(0x000000);
     this.renderer = new PIXI.WebGLRenderer( this.width, this.height,{antialias: true});
     this.renderer.backgroundColor = 0x000000;
+    this.renderer.view.className = 'resize-drag';
+
     console.log(this.renderer);
+
+    interact('.resize-drag')
+      .draggable({
+        onmove: window.dragMoveListener
+      })
+      .resizable({
+        preserveAspectRatio: true,
+        edges: { left: true, right: true, bottom: true, top: true }
+      })
+      .on('resizemove', function (event) {
+        var target = event.target,
+            x = (parseFloat(target.getAttribute('data-x')) || 0),
+            y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+        // update the element's style
+        target.style.width  = event.rect.width + 'px';
+        target.style.height = event.rect.height + 'px';
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
+
+        target.style.webkitTransform = target.style.transform =
+            'translate(' + x + 'px,' + y + 'px)';
+
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+        target.textContent = Math.round(event.rect.width) + '×' + Math.round(event.rect.height);
+      });
   }
 
   /**
